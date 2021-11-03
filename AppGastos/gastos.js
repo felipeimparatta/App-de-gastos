ff8e0a75df900df006fcbe75a900757400bc8a87
felipeimparatta <felipeimparatta@hotmail.com>
2021-11-03 14:49:49 -0300

    App de gastos

---
+++ AppGastos/gastos.js
@@ -0,0 +1,10 @@
+// cancelar la recagada de pagina
+const form = document.getElementById ("transactionForm");
+
+form.addEventListener("submit", function(event) {
+event.preventDefault();
+
+let transactionFormData = new FormData(form)
+
+})
+
